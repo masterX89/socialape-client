d6c3b898e358ef629b42eefb9654d80f87c99f40
hk.xia <595424932@qq.com>
2019-07-09 16:30:03 +0800

    Updates

--- precache-manifest.16c5fd216f3d78da466ecf9e2a4bdaa8.js
+++ precache-manifest.16c5fd216f3d78da466ecf9e2a4bdaa8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b35d8e94c702873cb685c767cf68cfc",
+    "revision": "c1285598d05cac27f08129430158e500",
     "url": "/socialape-client/index.html"
   },
   {
-    "revision": "b5fbc5f62e926d96cb18",
+    "revision": "14740116e04da005e84c",
     "url": "/socialape-client/static/css/main.a4e08f61.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/socialape-client/static/js/2.86f8ca84.chunk.js"
   },
   {
-    "revision": "b5fbc5f62e926d96cb18",
-    "url": "/socialape-client/static/js/main.ca367c3b.chunk.js"
+    "revision": "14740116e04da005e84c",
+    "url": "/socialape-client/static/js/main.868cbe7f.chunk.js"
   },
   {
     "revision": "f18095e5822104384be9",
