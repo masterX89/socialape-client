ac9bd14d37a29bd8ae66b15683f9c331c666892f
hk.xia <595424932@qq.com>
2019-07-10 14:30:29 +0800

    修改图标 增加侧边栏 增加Home、Login、Signup界面 修改App.css 增加router和MUI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Navbar from './components/Navbar';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Router basename='/socialape-client'>
+                    <Navbar/>
+                    <div className='container'>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/login" component={Login}/>
+                            <Route path="/signup" component={Signup}/>
+                        </Switch>
+                    </div>
+                </Router>
+            </div>
+
+        )
+    }
 }
 
 export default App;
