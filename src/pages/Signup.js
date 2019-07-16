a6212740cf67f0b364ee9186b356790e8d580afc
hk.xia <595424932@qq.com>
2019-07-16 11:20:44 +0800

    add signup redux

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropType from 'prop-types';
+import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.jpg';
 import axios from 'axios';
 import {FIREBASE_CORE_HOST} from '../constants/Constants';
@@ -11,7 +11,11 @@ import Grid from '@material-ui/core/Grid';
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import CircularProgress from "@material-ui/core/CircularProgress"
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+// Redux stuff
+import {connect} from 'react-redux';
+import {signupUser} from "../redux/actions/userActions";
 
 const styles = (theme) => ({
     form: theme.form,
@@ -30,36 +34,39 @@ class Signup extends Component {
             password: '',
             confirmPassword: '',
             handle: '',
-            loading: false,
             errors: {}
         };
     }
 
+    componentWillReceiveProps(nextProps, nextContext) {
+        if (nextProps.UI.errors) {
+            this.setState({errors: nextProps.UI.errors});
+        }
+    }
+
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({
-            loading: true
-        });
         const newUserData = {
             email: this.state.email,
             password: this.state.password,
             confirmPassword: this.state.confirmPassword,
             handle: this.state.handle
         };
-        axios.post(`${FIREBASE_CORE_HOST}/signup`, newUserData)
-            .then(res => {
-                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                })
-            })
+        // axios.post(`${FIREBASE_CORE_HOST}/signup`, newUserData)
+        //     .then(res => {
+        //         localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
+        //         this.setState({
+        //             loading: false
+        //         });
+        //         this.props.history.push('/');
+        //     })
+        //     .catch(err => {
+        //         this.setState({
+        //             errors: err.response.data,
+        //             loading: false
+        //         })
+        //     })
+        this.props.signupUser(newUserData, this.props.history);
     };
     handleChange = (event) => {
         this.setState({
@@ -68,8 +75,8 @@ class Signup extends Component {
     };
 
     render() {
-        const {classes} = this.props;
-        const {errors, loading} = this.state;
+        const {classes, UI:{loading}} = this.props;
+        const {errors} = this.state;
         return (
             <Grid container className={classes.form}>
                 <Grid item sm/>
@@ -113,7 +120,15 @@ class Signup extends Component {
 }
 
 Signup.propTypes = {
-    classes: PropType.object.isRequired
+    classes: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired,
+    signupUser: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(Signup);
+const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+});
+
+export default connect(mapStateToProps, {signupUser})(withStyles(styles)(Signup));
