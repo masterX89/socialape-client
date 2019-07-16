9430f424a78fc6c0f514247d9a97bfb928a20911
hk.xia <595424932@qq.com>
2019-07-16 17:36:13 +0800

    Updates

--- precache-manifest.c2a350c95ed3631f8d2abb8f2d29d52c.js
+++ precache-manifest.c2a350c95ed3631f8d2abb8f2d29d52c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d24a9481bd8f52c3f8ca773c368bf3e",
+    "revision": "8dba21aedc718d5e1ce86e24a148c679",
     "url": "/socialape-client/index.html"
   },
   {
-    "revision": "6618a455354e203c2265",
+    "revision": "126b8d55a3bcf83993e1",
     "url": "/socialape-client/static/css/main.b0673c64.chunk.css"
   },
   {
-    "revision": "2f8a17db67da7bdc4c04",
-    "url": "/socialape-client/static/js/2.7a3cb91e.chunk.js"
+    "revision": "e0d99c966b14ba9832a6",
+    "url": "/socialape-client/static/js/2.1bd37d4f.chunk.js"
   },
   {
-    "revision": "6618a455354e203c2265",
-    "url": "/socialape-client/static/js/main.34cabc58.chunk.js"
+    "revision": "126b8d55a3bcf83993e1",
+    "url": "/socialape-client/static/js/main.9f3ad034.chunk.js"
   },
   {
     "revision": "f18095e5822104384be9",
